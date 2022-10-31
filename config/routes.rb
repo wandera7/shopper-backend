3faa095d8c239ed3afc0c437721d609d7c08c723
wandera7 <julius.wandera@student.moringaschool.com>
2022-10-31 16:04:04 +0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
 end
